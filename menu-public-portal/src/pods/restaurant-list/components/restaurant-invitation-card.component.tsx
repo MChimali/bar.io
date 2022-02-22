90065c4bcedc96dc3c082fffeceaf5ac142e967b
manudous <you@example.com>
2022-02-22 10:29:23 +0100

    create reaturant-invitation-card.component

---
+++ menu-public-portal/src/pods/restaurant-list/components/restaurant-invitation-card.component.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { useTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import EmailIcon from "@mui/icons-material/Email";
+import * as classes from "../restaurant-list.styles";
+
+const AboutUs: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <div className={classes.credits(theme)}>
+      <Link href="#">
+        <a target="_blank" className={classes.anchor}>
+          <Typography
+            variant="caption"
+            component="h5"
+            className={classes.aboutus}
+          >
+            quienes somos
+          </Typography>
+        </a>
+      </Link>
+      <Typography
+        variant="caption"
+        component="h5"
+        className={classes.gastrocarta}
+      >
+        gastrocarta.net
+      </Typography>
+    </div>
+  );
+};
+
+const InfoMail: React.FC = () => {
+  const theme = useTheme();
+  return (
+    <div className={classes.info(theme)}>
+      <a href="mailto:info@gastrobar.net" target="_blank">
+        <EmailIcon className={classes.emailIcon(theme)} />
+      </a>
+      <Typography variant="body2" component="h2">
+        Si eres el propiertario de un establecimiento puedes publicar tu carta
+        de forma gratuita, ponte en contacto con nosotros en{" "}
+        <a
+          href="mailto:info@gastrobar.net"
+          target="_blank"
+          className={classes.emailSend}
+        >
+          info@gastrobar.net
+        </a>
+      </Typography>
+    </div>
+  );
+};
+
+export const RestaurantInvitationCard: React.FC = () => {
+  return (
+    <div className={classes.creditsContainer}>
+      <AboutUs />
+      <InfoMail />
+    </div>
+  );
+};
--- menu-public-portal/src/pods/restaurant-list/restaurant-list.component.tsx
+++ menu-public-portal/src/pods/restaurant-list/restaurant-list.component.tsx
@@ -1,118 +1,20 @@
-import Link from "next/link";
-import Head from "next/head";
-import { routes } from "core/router";
-import Typography from "@mui/material/Typography";
 import { RestaurantInfo } from "./restaurant-list.vm";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import EmailIcon from "@mui/icons-material/Email";
-import Button from "@mui/material/Button";
-import * as classes from "./restaurant-list.styles";
-import { ThemeProvider } from "@mui/material/styles";
-import { chooseTheme } from "core/theme";
-import { useTheme } from "@mui/material/styles";
-import { CardContentComponent } from "./components/cardContent.component";
-
-
+import { RestaurantCardComponent } from "./components/restaurant-card.component";
+import { RestaurantInvitationCard } from "./components/restaurant-invitation-card.component";
 
 interface Props {
   restaurantList: RestaurantInfo[];
 }
 
-const RestaurantList: React.FC<Props> = (props) => {
-  const theme = useTheme();
+export const RestaurantList: React.FC<Props> = (props) => {
   const { restaurantList } = props;
-  const restaurantElements = restaurantList.map((restaurant) => {
-    const {
-      name,
-      urlName,
-      address,
-      description,
-      locationUrl,
-      phone,
-      theme: themeName,
-    } = restaurant;
-    return (
-      <ThemeProvider theme={chooseTheme(themeName)} key={name}>
-        <div className={classes.Container}>
-          <Card className={classes.cardContainer(theme)}>
-            <CardContentComponent
-              name={name}
-              address={address}
-              description={description}
-              locationUrl={locationUrl}
-              phone={phone}
-              urlName={urlName}
-            />
-            <CardActions>
-              <Button
-                variant="contained"
-                href={routes.restaurant(urlName)}
-                className={classes.buttonRestaurantList(theme)}
-              >
-                VER CARTA
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-      </ThemeProvider>
-    );
-  });
 
   return (
-    <div>
-      <Head>
-        <title>Tu Carta - Restaurantes</title>
-        <meta
-          name="description"
-          content="La carta de tu restaurante favorito"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div className={classes.creditsContainer}>
-          <div className={classes.credits(theme)}>
-            <Link href="#">
-              <a target="_blank" className={classes.anchor}>
-                <Typography
-                  variant="caption"
-                  component="h5"
-                  className={classes.aboutus}
-                >
-                  quienes somos
-                </Typography>
-              </a>
-            </Link>
-            <Typography
-              variant="caption"
-              component="h5"
-              className={classes.gastrocarta}
-            >
-              gastrocarta.net
-            </Typography>
-          </div>
-          <div className={classes.info(theme)}>
-            <a href="mailto:info@gastrobar.net" target="_blank">
-              <EmailIcon className={classes.emailIcon(theme)} />
-            </a>
-            <Typography variant="body2" component="h2">
-              Si eres el propiertario de un establecimiento puedes publicar tu
-              carta de forma gratuita, ponte en contacto con nosotros en{" "}
-              <a
-                href="mailto:info@gastrobar.net"
-                target="_blank"
-                className={classes.emailSend}
-              >
-                info@gastrobar.net
-              </a>
-            </Typography>
-          </div>
-        </div>
-        {restaurantElements}
-      </main>
-    </div>
+    <main>
+      <RestaurantInvitationCard />
+      {restaurantList.map((restaurant) => (
+        <RestaurantCardComponent restaurant={restaurant} />
+      ))}
+    </main>
   );
 };
-
-export { RestaurantList };
