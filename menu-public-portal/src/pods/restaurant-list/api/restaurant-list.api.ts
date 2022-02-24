1b97ccb3181ad3ab0b70da613ac651c756cf4181
manudous <you@example.com>
2022-02-24 14:27:00 +0100

    first commit

--- menu-public-portal/src/pods/restaurant-list/api/restaurant-list.api.ts
+++ menu-public-portal/src/pods/restaurant-list/api/restaurant-list.api.ts
@@ -1,11 +1,13 @@
 import { cacheBuster } from "common/tools";
 import { RestaurantApi } from "./restaurant-list.api.model";
+import restaurantList from "../../../../public/restaurantList.json";
 
 export const getRestaurantListFromModel = async (): Promise<
   RestaurantApi[]
 > => {
-  const uri = cacheBuster(`http://localhost:3000/restaurantList.json`);
+  // const uri = cacheBuster(`/restaurantList.json`);
 
-  const response = await fetch(uri);
-  return response.json();
+  // const response = await fetch(uri);
+  // return response.json();
+  return restaurantList as RestaurantApi[];
 };
--- menu-public-portal/src/pods/restaurant/api/restaurant.api.ts
+++ menu-public-portal/src/pods/restaurant/api/restaurant.api.ts
@@ -1,10 +1,11 @@
 import { RestaurantApi } from "./restaurant.api.model";
+import { envConstants } from 'common/constants';
 import { cacheBuster } from "common/tools";
 
 export const getRestaurantByName = async (
   restaurantName: string
 ): Promise<RestaurantApi> => {
-  const uri = cacheBuster(`http://localhost:3000/${restaurantName}.json`);
+  const uri = cacheBuster(`/${restaurantName}.json`);
   const response = await fetch(uri);
   return response.json();
 };
