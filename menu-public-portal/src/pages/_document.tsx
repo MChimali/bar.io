a26faa6283ad53f100f8024ad6a5346e8b04a563
nasdan <daniel.sanchez@lemoncode.net>
2022-02-25 09:40:00 +0100

    add document with emotion

--- menu-public-portal/src/pages/_document.tsx
+++ menu-public-portal/src/pages/_document.tsx
@@ -1,16 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
+
+const key = 'custom';
+const cache = createCache({ key });
+const { extractCritical } = createEmotionServer(cache);
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const originalRenderPage = ctx.renderPage;
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => App,
+        enhanceApp: (App) => (props) =>
+          (
+            <CacheProvider value={cache}>
+              <App {...props} />
+            </CacheProvider>
+          ),
       });
 
     const initialProps = await Document.getInitialProps(ctx);
+    const { ids, css } = extractCritical(initialProps.html);
 
-    return initialProps;
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style data-emotion={`${key} ${ids.join(' ')}`}>{css}</style>
+        </>
+      ),
+    };
   }
 
   render() {
