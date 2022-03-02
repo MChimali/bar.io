72fab795fcc433b089a96ead3beed4a0d6b12dd7
manudous <you@example.com>
2022-03-02 09:42:34 +0100

    mapRestaurantListFromModelToApiModel test

--- tools/src/pods/restaurant/restaurant.mappers.spec.ts
+++ tools/src/pods/restaurant/restaurant.mappers.spec.ts
@@ -1,6 +1,10 @@
 import * as model from 'dals';
 import * as apiModel from './restaurant.api-model';
-import { reduceCategoryEntryListToRationDefinitionList } from './restaurant.mappers';
+import {
+  mapRestaurantListFromModelToApiModel,
+  reduceCategoryEntryListToRationDefinitionList,
+} from './restaurant.mappers';
+import { ObjectId } from 'mongodb';
 
 describe('restaurant.mapper', () => {
   describe('reduceCategoryEntryListToRationDefinitionList', () => {
@@ -154,5 +158,121 @@ describe('restaurant.mapper', () => {
         units: ['ration-type1', 'ration-type2'],
       },
     ] as model.RationDefinition[]);
+  });
+  describe('mapRestaurantListFromModelToApiModel', () => {
+    it('should return empty array when it feeds with undefined', () => {
+      // Arrange
+      const data: model.Restaurant[] = undefined;
+      // Act
+      const result: apiModel.RestaurantApiModel[] =
+        mapRestaurantListFromModelToApiModel(data);
+      // Assert
+      expect(result).toEqual([]);
+    });
+    it('should return empty array when it feeds with null', () => {
+      // Arrange
+      const data: model.Restaurant[] = null;
+      // Act
+      const result: apiModel.RestaurantApiModel[] =
+        mapRestaurantListFromModelToApiModel(data);
+      // Assert
+      expect(result).toEqual([]);
+    });
+    it('should return empty array when it feeds with empty array', () => {
+      // Arrange
+      const data: model.Restaurant[] = [];
+      // Act
+      const result: apiModel.RestaurantApiModel[] =
+        mapRestaurantListFromModelToApiModel(data);
+      // Assert
+      expect(result).toEqual([]);
+    });
+    it('should return an item when it feeds with a restaurant', () => {
+      // Arrange
+      const data: model.Restaurant[] = [
+        {
+          _id: new ObjectId('621f2c9f11f61c3eb1471ae6'),
+          name: 'name-test',
+          urlName: 'urlName-test',
+          phone: 'phone-test',
+          address: 'address-test',
+          locationUrl: 'locationUrl-test',
+          menuDate: new Date('2022-03-02'),
+          communitySourceUrl: 'communitySourceUrl-test',
+          official: false,
+          description: 'description-test',
+          theme: 'default',
+          rationsDefinitions: [],
+          menu: [
+            {
+              categoryName: 'category-test',
+              items: [
+                {
+                  name: 'item-test1',
+                  priceByRation: {
+                    rationName: 'ration-test',
+                    rationsTypes: [
+                      {
+                        unit: 'ration-type1',
+                        price: 2,
+                      },
+                      {
+                        unit: 'ration-type2',
+                        price: 2,
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ];
+      // Act
+      const result: apiModel.RestaurantApiModel[] =
+        mapRestaurantListFromModelToApiModel(data);
+      // Assert
+      expect(result).toEqual([
+        {
+          id: '621f2c9f11f61c3eb1471ae6',
+          name: 'name-test',
+          urlName: 'urlName-test',
+          phone: 'phone-test',
+          address: 'address-test',
+          locationUrl: 'locationUrl-test',
+          menuDate: new Date('2022-03-02'),
+          communitySourceUrl: 'communitySourceUrl-test',
+          official: false,
+          description: 'description-test',
+          theme: 'default',
+          menu: [
+            {
+              categoryName: 'category-test',
+              items: [
+                {
+                  name: 'item-test1',
+                  description: undefined,
+                  price: null,
+                  unit: null,
+                  priceByRation: {
+                    rationName: 'ration-test',
+                    rationsTypes: [
+                      {
+                        unit: 'ration-type1',
+                        price: 2,
+                      },
+                      {
+                        unit: 'ration-type2',
+                        price: 2,
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ] as apiModel.RestaurantApiModel[]);
+    });
   });
 });
