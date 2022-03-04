bf00a9a72755a60adbd38d450999c1b93e7f01d5
manudous <you@example.com>
2022-03-04 09:44:39 +0100

    add pagination db-repository

--- tools/src/dals/restaurant/repositories/restaurant.db-repository.ts
+++ tools/src/dals/restaurant/repositories/restaurant.db-repository.ts
@@ -4,8 +4,32 @@ import { getRestaurantContext } from './restaurant.context';
 import { ObjectId } from 'mongodb';
 
 export const dbRepository: RestaurantRepository = {
-  getRestaurantList: async (): Promise<Restaurant[]> => {
+  getRestaurantList: async (
+    page: number,
+    pageSize: number
+  ): Promise<Restaurant[]> => {
     try {
+      if (page && pageSize) {
+        const skip = page * pageSize - pageSize;
+        return getRestaurantContext()
+          .find(
+            {},
+            {
+              projection: {
+                name: 1,
+                urlName: 1,
+                phone: 1,
+                address: 1,
+                locationUrl: 1,
+                description: 1,
+                theme: 1,
+              },
+            }
+          )
+          .skip(skip)
+          .limit(pageSize)
+          .toArray();
+      }
       return await getRestaurantContext()
         .find(
           {},
