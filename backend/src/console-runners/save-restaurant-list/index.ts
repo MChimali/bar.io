98072a7adcf094bdcea6b80a8345a72c234b306f
nasdan <daniel.sanchez@lemoncode.net>
2022-03-10 14:44:00 +0100

    check stop job

--- backend/src/console-runners/save-restaurant-list/index.ts
+++ backend/src/console-runners/save-restaurant-list/index.ts
@@ -4,20 +4,27 @@ import { mapRestaurantFromApiModelToModel } from 'pods/restaurant/restaurant.map
 import { restaurantRepository } from 'dals';
 
 export const run = async (connectionString: string) => {
-  const mongoURI = connectionString || process.argv[8];
-  await connectToDBServer(mongoURI);
+  try {
+    const mongoURI = connectionString || process.argv[8];
+    await connectToDBServer(mongoURI);
 
-  const restaurantNames = fs.readdirSync(`${__dirname}/../restaurant-list`);
+    const restaurantNames = fs.readdirSync(`${__dirname}/../restaurant-list`);
 
-  for (const item in restaurantNames) {
-    const {
-      restaurant,
-    } = require(`../restaurant-list/${restaurantNames[item]}`);
-    await restaurantRepository.saveRestaurant(
-      mapRestaurantFromApiModelToModel(restaurant)
-    );
-    console.log(`${restaurantNames[item]} is saved`);
+    for (const item in restaurantNames) {
+      const {
+        restaurant,
+      } = require(`../restaurant-list/${restaurantNames[item]}`);
+      const modelRestaurant = mapRestaurantFromApiModelToModel(restaurant);
+      await restaurantRepository.saveRestaurant({
+        ...modelRestaurant,
+        // _id: undefined,
+      });
+      console.log(`${restaurantNames[item]} is saved`);
+    }
+
+    await disconnectFromDbServer();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
   }
-
-  await disconnectFromDbServer();
 };
