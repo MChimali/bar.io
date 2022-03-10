7ce8119ced583c38738bcc629bc8f9571d5c1e94
nasdan <daniel.sanchez@lemoncode.net>
2022-03-10 13:52:38 +0100

    fix env constant

--- backend/src/core/constants/env.constants.ts
+++ backend/src/core/constants/env.constants.ts
@@ -1,6 +1,6 @@
 export const envConstants = {
   isProduction: process.env.NODE_ENV === 'production',
-  PORT: process.env.PORT,
+  PORT: process.env.INTERNAL_PORT,
   STATIC_FILES_PATH: process.env.STATIC_FILES_PATH,
   isApiMock: process.env.API_MOCK === 'true',
   MONGODB_URI: process.env.MONGODB_URI,
